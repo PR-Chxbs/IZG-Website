decea992a164703989923af56e0b5fdc2a003e45
Wahina Chabalala <wp.chabalala@gmail.com>
2025-10-26 12:41:08 +0200

    feat: improve event logic readability

--- js/events.js
+++ js/events.js
@@ -4,10 +4,33 @@ document.addEventListener("DOMContentLoaded", async () => {
   const container = document.getElementById("event-cards");
 
   try {
-    const response = await fetch("https://izg-backend.onrender.com/api/events");
-    let events = await response.json();
-    var eventCounter = 0;
+    let eventCounter = 0;
+    
+    let events = await fetchEvents();
 
+    // 🔹 Keep only the first three events
+    events = events.length > 3 ? events.slice(0, 3) : events;
+
+    events.forEach(event => {
+      eventCounter += 1;
+
+      renderEventCard(event, container, eventCounter)
+      
+    });
+
+    if (events.length === 0) {
+      container.innerHTML = "<p>No events available.</p>";
+    }
+  } catch (err) {
+    container.innerHTML = "<p>Failed to load events. Please try again later.</p>";
+    console.error(err);
+  }
+});
+
+const fetchEvents = async () => {
+  const response = await fetch("https://izg-backend.onrender.com/api/events");
+    let events = await response.json();
+    
     console.log("loading");
     console.log(events);
 
@@ -20,46 +43,35 @@ document.addEventListener("DOMContentLoaded", async () => {
     // 🔹 Sort events by event_date (descending: latest first)
     events.sort((a, b) => b.event_date - a.event_date);
 
-    // 🔹 Keep only the first three events
-    events = events.length > 3 ? events.slice(0, 3) : events;
+    return events;
+}
 
-    events.forEach(event => {
-      eventCounter += 1;
+const renderEventCard = (eventData, parentClass, eventCounter) => {
+  const card = document.createElement("a");
+  const imageUrl = eventData.image_url ? eventData.image_url : "../resources/default-event.jpeg";
+  card.href = `./pages/event_details.html?eventId=${eventData.id}`;
+  card.className = eventCounter == 2 ? "event-card middle" : "event-card";
 
-      const card = document.createElement("a");
-      const imageUrl = event.image_url ? event.image_url : "../resources/default-event.jpeg";
-      card.href = `./pages/event_details.html?eventId=${event.id}`;
-      card.className = eventCounter == 2 ? "event-card middle" : "event-card";
+  const formattedDate = eventData.event_date.toLocaleDateString("en-ZA", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
 
-      const formattedDate = event.event_date.toLocaleDateString("en-ZA", {
-        weekday: "long",
-        year: "numeric",
-        month: "long",
-        day: "numeric",
-      });
+  card.innerHTML = `
+    <img src=${imageUrl} alt="${eventData.name}" class="event-image"/>
+    <div class="event-content">
+      <div class="event-details">
+        <p class="date">${formattedDate}</p>
+        <p class="location">${eventData.location}</p>
+      </div>
+      <div class="content">
+        <p class="title">${eventData.name}</p>
+        <p class="subtext">${eventData.description}</p>
+      </div>
+    </div>
+  `;
 
-      card.innerHTML = `
-        <img src=${imageUrl} alt="${event.name}" class="event-image"/>
-        <div class="event-content">
-          <div class="event-details">
-            <p class="date">${formattedDate}</p>
-            <p class="location">${event.location}</p>
-          </div>
-          <div class="content">
-            <p class="title">${event.name}</p>
-            <p class="subtext">${event.description}</p>
-          </div>
-        </div>
-      `;
-
-      container.appendChild(card);
-    });
-
-    if (events.length === 0) {
-      container.innerHTML = "<p>No events available.</p>";
-    }
-  } catch (err) {
-    container.innerHTML = "<p>Failed to load events. Please try again later.</p>";
-    console.error(err);
-  }
-});
+  parentClass.appendChild(card);
+}
